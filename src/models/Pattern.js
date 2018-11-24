71579139d53736354c6091943e575b8a026063ff
Julio C Vila <jcvilap@users.noreply.github.com>
2018-11-24 09:15:17 -0500

    Update Pattern.js

--- src/models/Pattern.js
+++ src/models/Pattern.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Pattern = new mongoose.Schema({
   query: {type: String, required: true},
+  name: String,
 });
 
 module.exports = mongoose.model('Pattern', Pattern);
